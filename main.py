09fdde5883390bee98d56cb5e6eba4d38917cb2e
Ethan Almeida <ethanalmeida@gmail.com>
2021-06-22 23:19:06 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import webbrowser
+from tkinter import ttk
+from tkinter import *
+
+
+
+
+
 
 def rickity():
     site = 'youtube.com/watch?v=dQw4w9WgXcQ'
@@ -6,5 +13,33 @@ def rickity():
     webbrowser.open(visit)
 
 
+root = Tk()
+
+
+root.geometry('597x369')
+root.configure(background='#F0F8FF')
+root.title('Rickety Browser')
+
+
+Label(root, text='Rickety Browser', bg='#F0F8FF', font=('arial', 20, 'bold')).place(x=206, y=11)
+
+
+
+URL=ttk.Entry(root, width=60)
+URL.place(x=66, y=130)
+
+
+
+
+Button(root, text='Search', bg='#F0F8FF', font=('arial', 15, 'bold'), command=rickity).place(x=450, y=115)
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
-    rickity()
+    root.mainloop()
